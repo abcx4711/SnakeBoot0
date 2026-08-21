b8533e3aa7fc9e2b44e22e18c394258b650a796d
nelbr <nwaissman@yahoo.com.br>
2026-08-21 12:25:34 +0200

    Turn off drive motor on any slot. Fix makesnakedisk.sh

--- source/makesnakedisk.sh
+++ source/makesnakedisk.sh
@@ -4,7 +4,7 @@
 dd if=/dev/zero of=disk/snake.dsk bs=256 count=560
 
 # Put $00 + your 255-byte program into sector 0
-dd if=/dev/zero dd of=disk/snake.dsk bs=1 count=1 conv=notrunc
+dd if=/dev/zero of=disk/snake.dsk bs=1 count=1 conv=notrunc
 dd if=bin/snake.bin of=disk/snake.dsk bs=1 seek=1 conv=notrunc
 
 
